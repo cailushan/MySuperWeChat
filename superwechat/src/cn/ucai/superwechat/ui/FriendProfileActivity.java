6ccd253e73bcc44a304e2fcfc01724a3d50e2f9c
cls <731058610@qq.com>
2017-02-14 10:16:16 +0800

    好友详细界面的数据显示

---
+++ superwechat/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -0,0 +1,97 @@
+package cn.ucai.superwechat.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hyphenate.easeui.domain.User;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.SuperWeChatHelper;
+import cn.ucai.superwechat.utils.MFGT;
+
+public class FriendProfileActivity extends Activity {
+
+    @BindView(R.id.img_back)
+    ImageView imgBack;
+    @BindView(R.id.txt_title)
+    TextView txtTitle;
+    @BindView(R.id.profile_image)
+    ImageView profileImage;
+    @BindView(R.id.tv_userinfo_nick)
+    TextView tvUserinfoNick;
+    @BindView(R.id.tv_userinfo_name)
+    TextView tvUserinfoName;
+    @BindView(R.id.btn_add_contact)
+    Button btnAddContact;
+    @BindView(R.id.btn_send_msg)
+    Button btnSendMsg;
+    @BindView(R.id.btn_send_video)
+    Button btnSendVideo;
+    User user = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_profile);
+        ButterKnife.bind(this);
+        initData();
+    }
+
+    private void initData() {
+        imgBack.setVisibility(View.VISIBLE);
+        txtTitle.setVisibility(View.VISIBLE);
+        txtTitle.setText(R.string.userinfo_txt_title);
+        user = (User) getIntent().getSerializableExtra(I.User.USER_NAME);
+        if (user != null) {
+            showUserInfo();
+        } else {
+            MFGT.finish(this);
+        }
+    }
+
+    private void showUserInfo() {
+        tvUserinfoNick.setText(user.getMUserNick());
+        EaseUserUtils.setAppUserAvatarByPath(this, user.getAvatar(), profileImage);
+        tvUserinfoName.setText("微信号: " + user.getMUserName());
+        if (isFriend()) {
+            btnSendMsg.setVisibility(View.VISIBLE);
+            btnSendVideo.setVisibility(View.VISIBLE);
+        } else {
+            btnAddContact.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private boolean isFriend() {
+        User u = SuperWeChatHelper.getInstance().getAppContactList().get(user.getMUserName());
+        if (u == null) {
+            return false;
+        } else {
+            SuperWeChatHelper.getInstance().saveAppContact(user);
+            return true;
+        }
+    }
+
+    @OnClick({R.id.img_back, R.id.btn_add_contact, R.id.btn_send_msg, R.id.btn_send_video})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_back:
+                MFGT.finish(this);
+                break;
+            case R.id.btn_add_contact:
+                break;
+            case R.id.btn_send_msg:
+                break;
+            case R.id.btn_send_video:
+                break;
+        }
+    }
+}
--- superwechat/src/cn/ucai/superwechat/utils/MFGT.java
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -6,8 +6,10 @@ import android.support.v4.app.FragmentActivity;
 
 import com.hyphenate.easeui.domain.User;
 
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.ui.AddContactActivity;
+import cn.ucai.superwechat.ui.FriendProfileActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.MainActivity;
 import cn.ucai.superwechat.ui.RegisterActivity;
@@ -62,7 +64,7 @@ public class MFGT {
         startActivity(activity, AddContactActivity.class);
     }
 
-    public static void gotoFirent(Activity activity, User user) {
-
+    public static void gotoFriend(Activity activity, User user) {
+        startActivity(activity, new Intent(activity, FriendProfileActivity.class).putExtra(I.User.USER_NAME, user));
     }
 }
