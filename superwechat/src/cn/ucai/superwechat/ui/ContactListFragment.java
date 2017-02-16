1a06d4fee1e89fc9f1522c7db66fc70c55435282
cls <731058610@qq.com>
2017-02-16 10:32:57 +0800

    删除联系人和删除添加到黑名单的对话框

--- superwechat/src/cn/ucai/superwechat/ui/ContactListFragment.java
+++ superwechat/src/cn/ucai/superwechat/ui/ContactListFragment.java
@@ -22,7 +22,12 @@ import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.db.UserDao;
+import cn.ucai.superwechat.domain.Result;
+import cn.ucai.superwechat.net.NetDao;
+import cn.ucai.superwechat.net.OnCompleteListener;
+import cn.ucai.superwechat.utils.L;
 import cn.ucai.superwechat.utils.MFGT;
+import cn.ucai.superwechat.utils.ResultUtils;
 import cn.ucai.superwechat.widget.ContactItemView;
 
 import com.hyphenate.easeui.domain.EaseUser;
@@ -216,9 +221,6 @@ public class ContactListFragment extends EaseContactListFragment {
                 e.printStackTrace();
             }
             return true;
-        } else if (item.getItemId() == R.id.add_to_blacklist) {
-            moveToBlacklist(toBeProcessUsername);
-            return true;
         }
         return super.onContextItemSelected(item);
     }
@@ -236,6 +238,34 @@ public class ContactListFragment extends EaseContactListFragment {
         pd.setMessage(st1);
         pd.setCanceledOnTouchOutside(false);
         pd.show();
+        NetDao.removeContact(getContext(), EMClient.getInstance().getCurrentUser(), tobeDeleteUser.getMUserName(), new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                if (s != null) {
+                    L.e(TAG,"s ="+s);
+                    Result result = ResultUtils.getResultFromJson(s, User.class);
+                    if (result != null && result.isRetMsg()) {
+                        // remove user from memory and database
+                        UserDao dao = new UserDao(getActivity());
+                        dao.deleteAppContact(tobeDeleteUser.getMUserName());
+                        SuperWeChatHelper.getInstance().getAppContactList().remove(tobeDeleteUser.getMUserName());
+                        getActivity().runOnUiThread(new Runnable() {
+                            public void run() {
+                                pd.dismiss();
+                                contactList.remove(tobeDeleteUser);
+                                contactListLayout.refresh();
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
         new Thread(new Runnable() {
             public void run() {
                 try {
